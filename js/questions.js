1d73e7d056bc6f89fa2e0df6798c36ad01df6780
Ontarioku <148642510+Ontarioku@users.noreply.github.com>
2023-11-22 11:18:42 -0400

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+const quizQuestions = [
+    {
+        id: 1,
+        question: "How many vertebrae are in a giraffe's neck?",
+        choices: [
+            "12",
+            "100",
+            "7",
+            "55"
+        ],
+        answer: "7"
+    },
+    {
+        id: 2,
+        question: "Which mammal lays eggs?",
+        choices: [
+            "Ostrich",
+            "Platypus",
+            "Tiger",
+            "Rhinoceros"
+        ],
+        answer: "Platypus"
+    },
+    {
+        id: 3,
+        question: "What allows cephalopods to change colour?",
+        choices: [
+            "Chromatophores",
+            "Internal Bacteria",
+            "Colour Cells",
+            "Chlorophores"
+        ],
+        answer: "Chromatophores"
+    },
+    {
+        id: 4,
+        question: "How much does a blue whale's heart weigh?",
+        choices: [
+            "200 lbs",
+            "400 lbs",
+            "500 lbs",
+            "1 ton"
+        ],
+        answer: "400 lbs"
+    },
+    {
+        id: 5,
+        question: "What is another name for a shark egg case?",
+        choices: [
+            "Pirate's Chest",
+            "Sailor's Stash",
+            "Mermaid's Purse",
+            "Neptune's Beard"
+        ],
+        answer: "Mermaid's Purse"
+    },
+    {
+        id: 6,
+        question: "What do gentoo and adelie peguins offer to their mates?",
+        choices: [
+            "Pebbles",
+            "Fish",
+            "Feathers",
+            "Bones"
+        ],
+        answer: "Pebbles"
+    },
+    {
+        id: 7,
+        question: "What do you call a group of crows?",
+        choices: [
+            "Cuddle",
+            "Murder",
+            "Crash",
+            "Unkindness"
+        ],
+        answer: "Murder"
+    },
+    
+    {
+        id: 8,
+        question: "What type of nonhuman mammal can keep a beat?",
+        choices: [
+            "Elephants",
+            "Dogs",
+            "Sea Lions",
+            "Bats"
+        ],
+        answer: "Sea Lions"
+    }, 
+    {
+        id: 9,
+        question: "What is the gestation period for dogs",
+        choices: [
+            "40-60 days",
+            "58-68 days",
+            "60-80 days",
+            "88-108 days"
+        ],
+        answer: "58-68 days"
+    },
+    {
+        id: 10,
+        question: "Which animal's blood is often used in medical products?",
+        choices: [
+            "Shrews",
+            "Horseshoe Crabs",
+            "Scorpions",
+            "Rabbits"
+        ],
+        answer: "Horseshoe Crabs"
+    }
+];
